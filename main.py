9c2b6d679c4e5cf469e34a8d37e98dc0e1582e00
usk39 <60925409+usk39@users.noreply.github.com>
2020-04-22 14:17:09 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, abort
+
+from linebot import (
+   LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+   InvalidSignatureError
+)
+from linebot.models import (
+   MessageEvent, TextMessage, TextSendMessage,
+)
+
+import os
+import hotel
+
+app = Flask(__name__)
+
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+   # get X-Line-Signature header value
+   signature = request.headers['X-Line-Signature']
+
+   # get request body as text
+   body = request.get_data(as_text=True)
+   app.logger.info("Request body: " + body)
+
+   # handle webhook body
+   try:
+       handler.handle(body, signature)
+   except InvalidSignatureError:
+       print("Invalid signature. Please check your channel access token/channel secret.")
+       abort(400)
+
+   return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    push_text = event.message.text
+    results = hotel.extract_words(push_text)
+    if isinstance(results, tuple):
+        msg = hotel.hotel_search(*results)
+    else:
+        msg = results
+    line_bot_api.replay_message(event.replay_token, TextSendMessage(text=msg))
+
+if __name__ == "__main__":
+   port = int(os.getenv("PORT"))
+   app.run(host="0.0.0.0", port=port)
